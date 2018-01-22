5b8caf6f508aa865d712aae81192503cd72cc446
adambartosiewicz <adam.bartosiewicz@docplanner.com>
2018-01-22 20:28:01 +0100

    Use v-on to update variable

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 new Vue({
 	el: '#app',
 	data: {
+		counter: 0,
 		title: 'Hello World!',
-		link: 'http://google.com',
-		finishedLink: '<a href="http://google.com">Google</a>'
+		finishedLink: '<a href="http://google.com">Google</a>',
+		link: 'http://google.com'
 	},
 	methods: {
 		sayHello: function () {
 			this.title = 'Hello';
 			return this.title;
+		},
+		increase: function () {
+			this.counter++;
 		}
 	}
 });
